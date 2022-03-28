d5463403748efb40fb13fbdb34761419b2a90f19
Danillo Ilggner <danilloilggner@gmail.com>
2022-03-28 02:55:28 -0300

    improved code

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const users = [];
 function checksExistsUserAccount(request, response, next) {
   const { username } = request.headers;
   const user = users.find((user) => user.username === username);
-  if (!user || !username) return response.status(404).send();
+  if (!user) return response.status(404).json({ error: "User not found" });
 
   request.user = user;
 
@@ -23,7 +23,7 @@ app.post("/users", (request, response) => {
   const { name, username } = request.body;
   const alreadyExists = users.some((user) => user.username == username);
 
-  if (!name || !username || alreadyExists) {
+  if (alreadyExists) {
     return response.status(400).json({ error: "User already exists" });
   }
 
@@ -36,17 +36,15 @@ app.post("/users", (request, response) => {
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
   const { todos } = request.user;
-
   return response.status(200).json(todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
   const { title, deadline } = request.body;
-  const { username } = request.user;
+  const { user } = request;
 
   if (!title || !deadline) return response.status(400).send();
 
-  const index = users.findIndex((user) => user.username === username);
   const todo = {
     id: uuidv4(),
     title: title,
@@ -54,55 +52,51 @@ app.post("/todos", checksExistsUserAccount, (request, response) => {
     deadline: new Date(deadline),
     created_at: new Date(),
   };
-  users[index].todos.push(todo);
 
+  user.todos.push(todo);
   return response.status(201).json(todo);
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  const { username } = request.user;
   const { title, deadline } = request.body;
+  const { user } = request;
   const { id } = request.params;
 
-  const indexUser = users.findIndex((user) => user.username === username);
-  const indexTodo = users[indexUser].todos.findIndex((todo) => todo.id === id);
+  const todo = user.todos.find((todo) => todo.id === id);
 
-  if (indexUser > -1 && indexTodo > -1) {
-    users[indexUser].todos[indexTodo].title = title;
-    users[indexUser].todos[indexTodo].deadline = new Date(deadline);
+  if (todo) {
+    todo.title = title;
+    todo.deadline = new Date(deadline);
 
-    return response.status(200).json(users[indexUser].todos[indexTodo]);
+    return response.status(200).json(todo);
   }
 
   return response.status(404).json({ error: "Todo dont exist" });
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  const { username } = request.user;
+  const { user } = request;
   const { id } = request.params;
 
-  const indexUser = users.findIndex((user) => user.username === username);
-  const indexTodo = users[indexUser].todos.findIndex((todo) => todo.id === id);
+  const todo = user.todos.find((todo) => todo.id === id);
 
-  if (indexUser > -1 && indexTodo > -1) {
-    users[indexUser].todos[indexTodo].done = true;
+  if (todo) {
+    todo.done = true;
 
-    return response.status(200).json(users[indexUser].todos[indexTodo]);
+    return response.status(200).json(todo);
   }
 
   return response.status(404).json({ error: "Todo dont exist" });
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  const { username } = request.user;
+  const { user } = request;
   const { id } = request.params;
 
-  const indexUser = users.findIndex((user) => user.username === username);
-  const indexTodo = users[indexUser].todos.findIndex((todo) => todo.id === id);
+  const todo = user.todos.find((todo) => todo.id === id);
 
-  if (indexUser > -1 && indexTodo > -1) {
-    users[indexUser].todos.splice(indexTodo);
-
+  if (todo) {
+    user.todos.splice(todo);
     return response.status(204).send();
   }
 
